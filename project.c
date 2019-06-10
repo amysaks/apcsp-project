f9b0d963a44e0a912ded2156c486e756aa363caa
amysaks <31449912+amysaks@users.noreply.github.com>
2019-06-09 21:38:20 -0700

    final code

--- project.c
+++ project.c
@@ -4,37 +4,22 @@
  
 int main()
 {
-   int SIZE;
-   struct name {
-	char name[50];};
-  struct name nameArr[SIZE];
-   int i;
-   char name[256];
-   int num;
+  int SIZE,j;
+  char name[50];
+  char names[SIZE][50];
   char input[256];
  
 printf("How many names would you like to enter\n");
-fgets(input, 256, stdin);
-sscanf(input, "%d", SIZE);
+scanf("%d",&SIZE);
 
 for (int i = 0; i < SIZE; i++){
-
-printf("Input Name %d\n", i + 1);
-fgets(input, 256, stdin);
-sscanf(input, "%s", name);
-strcpy(nameArr[i].name, name);
+  printf("Input Name %d\n", i+1);
+  scanf("%s", name);
+  strcpy(names[i], name);
 }
-
-  int c;
- 
-  printf("Random Baby Name/n");
- 
-  for (c = 1; c < SIZE; c++) {
-    i = rand() % 100 + 1;
-    printf("%s\n", nameArr[i]);
-  }
+printf("Random Baby Name \n");
+j = (rand() % SIZE+1);
+printf("%s\n",names[j]);
  
   return 0;
-
-
 }
